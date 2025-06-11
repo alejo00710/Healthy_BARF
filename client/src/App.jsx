bf07f38edcb65d8ce633cc9a969f8f3e1925bf77
alejo00710 <alejo007xd@gmail.com>
2025-06-11 16:50:05 -0500

    Resuelvo conflicto en App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,42 +10,31 @@ import CatalogoProducts from "./components/product/CatalogoProducts";
 import CartPage from './pages/CartPage';
 import CartWithPage from "./pages/CartWithPage";
 import PersonalDataPage from "./pages/PersonalDataPage";
-<<<<<<< HEAD
 import AdminPage from './pages/AdminPage';
-
-
-=======
 import EntregaPage from './pages/EntregaPage';
 import PagoPage from './pages/PagoPage';
 import DetalleCompraPage from './pages/DetalleCompra';
->>>>>>> fb8241e5243e982870999d038f4f277982b2af9b
-
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/editProfile" element={<ProfilePage />} />
+        <Route path="/favorite" element={<FavoritePage />} />
+        <Route path="/terminos" element={<TerminosPage />} />
         <Route path="/ProductInfo" element={<ProductPage />} />
         <Route path="/product/:id" element={<ProductPage />} />
         <Route path="/catalogo" element={<CatalogoProducts />} />
         <Route path="/carrito" element={<CartPage />} />
         <Route path="/carritoProduct" element={<CartWithPage />} />
         <Route path="/datos" element={<PersonalDataPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/editProfile" element={<ProfilePage />} />
-<<<<<<< HEAD
-        <Route path="/favorite" element={<FavoritePage />} />
-        <Route path="/terminos" element={<TerminosPage />} />
-        <Route path="/admin" element={<AdminPage />} />        
-=======
-        <Route path="/favorite" element={<FavoritePage />} />      
-        <Route path="/terminos" element={<TerminosPage />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path="/Entrega" element={<EntregaPage />} />
         <Route path="/Pago" element={<PagoPage />} />
         <Route path="/Detalle" element={<DetalleCompraPage />} />
->>>>>>> fb8241e5243e982870999d038f4f277982b2af9b
       </Routes>
     </Router>
   );
