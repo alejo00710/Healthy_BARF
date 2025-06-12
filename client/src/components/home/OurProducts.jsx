9ef346bbc76cf1a13a210638e942d51d2e6ce89e
Estefany <estefanymb046@gmail.com>
2025-06-11 21:40:49 -0500

    Se direccionaron las vistas

--- client/src/components/home/OurProducts.jsx
+++ client/src/components/home/OurProducts.jsx
@@ -3,34 +3,27 @@ import styles from '../../styles/home/OurProducts.module.css';
 import dogImage from '../../assets/images/perro.png';
 import catImage from '../../assets/images/gato1.png';
 import bgImage from '../../assets/images/Diseño sin título (2).png';
+import { Link } from 'react-router-dom';
 
 const OurProducts = () => {
   return (
-    <section 
-      className={styles.ourProducts} 
+    <section
+      className={styles.ourProducts}
       style={{ backgroundImage: `url(${bgImage})` }}
     >
       <div className="container">
         <h2 className={styles.sectionTitle1}>Nuestros Productos</h2>
-        
         <div className={styles.productsGrid1}>
-          <div className={`${styles.productCard1} ${styles.blueCard}`}>
-            <h3 className={styles.productTitle}>Alimento BARF</h3>
-            <div className={styles.productImage1}>
-              <img src={dogImage} alt="Para perros" className={styles.petImg} />
-              <img src={catImage} alt="Para gatos" className={styles.petImg} />
+          <Link to="/catalogo">
+            <div className={`${styles.productCard1} ${styles.blueCard}`}>
+              <h3 className={styles.productTitle}>Alimento BARF</h3>
+              <div className={styles.productImage1}>
+                <img src={dogImage} alt="Para perros" className={styles.petImg} />
+                <img src={catImage} alt="Para gatos" className={styles.petImg} />
+              </div>
             </div>
-          </div>
-          
-          <div className={`${styles.productCard1} ${styles.yellowCard}`}>
-            <h3 className={styles.productTitle}>Snacks</h3>
-            <div className={styles.productImage1}>
-              <img src={dogImage} alt="Para perros"className={styles.petImg}/>
-              <img src={catImage} alt="Para gatos" className={styles.petImg}/>
-            </div>
-          </div>
+          </Link>
         </div>
-        
         <div className={styles.viewAllContainer}>
           <a href="#" className={styles.viewAllBtn}>VER TODO</a>
         </div>
